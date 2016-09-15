a10fe69c1c9305b363b91a5baa4526cc231cf1fa
neolectron <neolectron@users.noreply.github.com>
2016-09-15 02:00:18 +0200

    fixed, since pull_requested fish repo :)

--- fix_fish.sh
+++ fix_fish.sh
@@ -1,27 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 #fix_fish.sh - mcartier
-fishpath="$HOME/.brew/Cellar/fish/2.3.1";
 
-#test the version
-if [ -d $fishpath ];
-then
-  echo "you have the good version";
-else
-  echo "your version isn't supported by this fix :/\nplease use fish 2.3.1";
-  exit;
-fi
-
-#backing up and fixing fish
-echo "backing up and fixing fish.";
-mv $fishpath/bin/fish $fishpath/bin/fish_bak
-curl -L https://raw.githubusercontent.com/neolectron/fish-shell/master/42hotfix/2.3.1/fish > $fishpath/bin/fish
-chmod +x $fishpath/bin/fish;
-
-#launch fish shell as default login shell
-if [ "$1" = "--login" ];
-  then
-    echo "set up fish as default login shell";
-    [ -f $HOME/.zshrc ] && mv $HOME/.zshrc $HOME/.zshrc_bak;
-    echo "exec fish -l" > .zshrc;
-fi
+brew install autoconf;
+printf "fish installation path : ";
+read FISH_DIR;
+eval FISH_DIR=$FISH_DIR;
+echo "installing fish in $FISH_DIR;"
+git clone https://github.com/fish-shell/fish-shell.git /tmp/fish;
+cd /tmp/fish && autoconf && ./configure --prefix="$FISH_DIR" SED=/usr/bin/sed&& make && make install;
+echo "DO NOT FORGET to add $FISH_DIR/bin to your path :)"
+echo "you can put 'fish -l' in your .zshrc file :)"
